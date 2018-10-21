9382d3ec8683c66cf04f4523964565703bf1a1ae
Jaskaran Singh <Jaskaran1.dce@gmail.com>
2018-10-22 00:51:16 +0530

    Frame is made

--- src/GameDevelopment/tictac.java
+++ src/GameDevelopment/tictac.java
@@ -1,5 +1,18 @@
+
 package GameDevelopment;
 
-public class tictac {
+import javax.swing.JFrame;
+
+//We are using swing framework here
+// for makin a frame we will be using a JFrame
+public class tictac extends JFrame {
+	
+	public tictac(){
+		
+		super.setTitle("TicTacToe");
+		super.setSize(800, 800);
+		super.setVisible(true); //make frame visible on the desktop.
+	}
+	
 
 }
